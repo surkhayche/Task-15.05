9ef07a2901c08c218bce7e22e89c1302ad52eef7
Surkhay <surkhayshikhiyev@gmail.com>
2024-05-15 02:32:20 +0400

    Task 2 required

---
+++ Task 15.05/Task 15.05/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Task_15._05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] numbersArray = { 1, 21, 30, 42, 5, 632, 7, 57, };
+            //int maxValue = numbersArray[0];
+
+            //for (int i = 0; i < numbersArray.Length; i++)
+            //{
+            //    if (maxValue < numbersArray[i])
+            //    {
+            //        maxValue = numbersArray[i];
+            //    }
+            //}
+            //Console.WriteLine("En boyuk eded"+ maxValue);
+
+            
+
+
+
+
+
+        }
+
+
+    }
+}
